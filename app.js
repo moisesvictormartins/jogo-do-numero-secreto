16bfe9dbae43c63c55eee39f06432639f82f3926
MoH-Oliver <moisesmartinstech@gmail.com>
2025-02-25 16:21:25 -0300

    Revert "deixando o jogo muito dificil"
    This reverts commit 2ad2b96b66a0b031dbb9137579645885c24cc59d.

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let listaDeNumerosSorteados = [];
-let numeroLimite = 500;
+let numeroLimite = 5;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
@@ -17,7 +17,7 @@ function exibirTextoNaTela(tag, texto) {
 }
 function exibirMensagemInicial() {
     exibirTextoNaTela('h1', 'Jogo do número secreto');
-    exibirTextoNaTela('p', 'Escolha um número entre 1 e 500');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 5');
 }
 exibirMensagemInicial();
 
